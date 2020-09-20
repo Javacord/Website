1177fccd58c136b315778d61234e21c862ae4fb8
Bastian Oppermann <bastianoppermann1997@gmail.com>
2020-09-20 17:54:40 +0200

    Add redirects for old urls used in README

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,2 +1,10 @@
 require("katex/dist/katex.min.css");
 require("katex/dist/katex.min.js");
+
+export default ({ router }) => {
+    router.addRoutes([
+        // These are still used in the README
+        { path: '/wiki/essential-knowledge/working-with-optionals/', redirect: '/wiki/essential-knowledge/optionals.html' },
+        { path: '/wiki/essential-knowledge/working-with-futures/', redirect: '/wiki/essential-knowledge/completable-futures.html' }
+    ])
+}
