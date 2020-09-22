cc4c1a462a340066ef8551c94a0fa676cbeb84dc
Bastian Oppermann <bastianoppermann1997@gmail.com>
2020-09-22 16:01:36 +0200

    Fix search index generation

--- plugins/bot-api/index.js
+++ plugins/bot-api/index.js
@@ -5,13 +5,15 @@ module.exports = (options, context) => ({
         const { outDir } = context;
         logger.wait('Generating search index for Discord bot...');
         const searchIndexPath = path.resolve(outDir, 'bot-search-index.json');
-        const data = context.pages.map(page => ({
-            title: page.title,
-            headers: page.headers,
-            content: page._strippedContent,
-            path: page.path,
-            keywords: (page.frontmatter || {}).keywords || []
-        }));
+        const data = context.pages
+            .filter(page => page.title && page._strippedContent)
+            .map(page => ({
+                title: page.title,
+                headers: page.headers,
+                content: page._strippedContent,
+                path: page.path,
+                keywords: (page.frontmatter || {}).keywords || []
+            }));
         await fs.writeFile(searchIndexPath, JSON.stringify(data));
     }
 })
