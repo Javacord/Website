0e392f9435ad39d5e169f703dee3d18db43a657d
Bastian Oppermann <bastianoppermann1997@gmail.com>
2020-09-22 19:41:30 +0200

    Fix redirect
    This is strange. The old redirect worked, but still showed
    a 404 page. After reloading the page, it displayed the
    correct page.

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -4,7 +4,7 @@ require("katex/dist/katex.min.js");
 export default ({ router }) => {
     router.addRoutes([
         // These are still used in the README
-        { path: '/wiki/essential-knowledge/working-with-optionals/', redirect: '/wiki/essential-knowledge/optionals.html' },
-        { path: '/wiki/essential-knowledge/working-with-futures/', redirect: '/wiki/essential-knowledge/completable-futures.html' }
+        { path: '/wiki/essential-knowledge/working-with-optionals/', redirect: '/wiki/essential-knowledge/optionals' },
+        { path: '/wiki/essential-knowledge/working-with-futures/', redirect: '/wiki/essential-knowledge/completable-futures' }
     ])
 }
